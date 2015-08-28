59d3ea3ddfd6a9cf95652be5a5e7b533c8d55d8c
Julian Samarjiev <julian.samarjiev@commercetools.de>
2015-08-28 22:42:47 +0300

    complete FriendListApp component, errors need to be resolved

--- src/containers/FriendListApp.js
+++ src/containers/FriendListApp.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { Component, PropTypes } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import * as FriendsActions from '../actions/FriendsActions';
+import { FriendList, AddFriendInput } from '../components';
 
-export default class FriendListApp extends React.Component {
+@connect(state => ({
+  friendlist: state.friendlist
+}))
+
+export default class FriendListApp extends Component {
+  static propTypes = {
+    friendsById: PropTypes.object.isRequired,
+    dispatch: PropTypes.func.isRequired
+  }
+
   render() {
+    const { friendlist: { friendsById }, dispatch } = this.props;
+    const actions = bindActionCreators(FriendsActions, dispatch);
+
     return (
-      <div>
-        Hello world!!!
+      <div className={styles.friendListApp}>
+        <h1>The FriendList</h1>
+        <AddFriendInput addFriend={actions.addFriend} />
+        <FriendList friends={friendsById} actions={actions} />
       </div>
-    )
+    );
   }
 }
